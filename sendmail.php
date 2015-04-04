d573f765447bd40c97245ad175b2378f0abc7f9f
Binzzzz <binoybalu1993@gmail.com>
2015-04-04 17:12:39 +0530

    text fix

--- sendmail.php
+++ sendmail.php
@@ -1,16 +1,7 @@
-<?php
-
-if(isset($_GET['name'])){
-$name = $_GET['name'];
-$email = $_GET['email'];
-$msg = $_GET['msg'];
-$to = "support@roboinventions.com";
-$subject = "Feedback mail";
-
-$message = "
+<?php if(isset($_GET[ 'name'])){ $name=$ _GET[ 'name']; $email=$ _GET[ 'email']; $msg=$ _GET[ 'msg']; $to="support@roboinventions.com" ; $subject="Feedback mail" ; $message="
 <html>
 <head>
-<title>HTML email</title>
+<title>FeedBack Mail From $name</title>
 </head>
 <body>
 <p>$msg</p>
@@ -26,31 +17,4 @@ $message = "
 </table>
 </body>
 </html>
-";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More header
-$headers .= 'From:' . "\r\n";
-$headers .= 'Cc:pratheesh@roboinventions.com,puneet@roboinventions.com,$email' . "\r\n";
-
-$mailer = mail($to,$subject,$message,$headers);
- 
-    if($mailer) {
-      echo '<script type="text/javascript">'
-   , 'swal("Your Feedback Is!", "Submitted!", "success");'
-   , '</script>';
-}
-    else{
-        
-        echo "Processing Error";
-    }
-    
-}
- else{
-     
-     echo  "No data recieved";
- }   
-?> 
+" ; $headers="MIME-Version: 1.0" . "\r\n"; $headers .="Content-type:text/html;charset=UTF-8" . "\r\n"; $headers .='From:' . "\r\n"; $headers .='Cc:pratheesh@roboinventions.com,puneet@roboinventions.com,$email,csk@roboinventions.com' . "\r\n"; $mailer=m ail($to,$subject,$message,$headers); if($mailer) { echo '<script type="text/javascript">' , 'swal("Your Feedback Is!", "Submitted!", "success");' , '</script>'; } else{ echo "Processing Error"; } } else{ echo "No data recieved"; } ?>
